b90dfd7e70df5aa68e8581ba885a8bff92073925
pablovercosa <pablovercosa@gmail.com>
2025-08-12 10:40:02 -0300

    limpeza

--- Dll_NFCe/Interface_register.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\regsvcs.exe NfceInterface.dll /tlb:NfceInterface.tlb 
-pause
--- Dll_NFCe/Interface_unregister.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\regsvcs -u NfceInterface.dll /tlb:NfceInterface.tlb
-pause
